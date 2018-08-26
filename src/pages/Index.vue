8a3be7455152b3a09262e11d4038e625f17e46b2
adad <adad@afaf.com>
2018-08-26 17:41:25 +0000

    lk

---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center">
+<q-tabs color='secondary' >
+        <q-tab slot="title" icon="mail" label="Mails" />
+        <q-tab slot="title" icon="alarm" label="Alarms" />
+        <q-tab slot="title" icon="movie" label="Movies" />
+        <q-tab slot="title" icon="mail" label="Mails" />
+        <q-tab slot="title" icon="alarm" label="Alarms" />
+        <q-tab slot="title" icon="movie" label="Movies" />
+        <q-tab slot="title" icon="mail" label="Mails" />
+      </q-tabs>  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
